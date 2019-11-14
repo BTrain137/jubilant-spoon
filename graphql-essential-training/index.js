9a86e8a8744d7990c4af755ecd4d17419af8140b
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-11-13 21:57:07 -0800

    import resolver

--- graphql-essential-training/index.js
+++ graphql-essential-training/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
+import resolver from './resolver';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
+
+const root = resolver;
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,

9e4d3ba378a646642282b2e6505cadd0e2e49661
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-11-24 11:34:12 -0800

    Add sql light into project

--- graphql-essential-training/index.js
+++ graphql-essential-training/index.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import schema from './data/schema';
+import { schema } from './data/schema';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
-
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,

f8d58b54df6ac005f5928b6a8396f66cc5e1eed0
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-12-03 22:09:01 -0800

    complete front end

--- react-apollo/client/src/src/Contacts.js
+++ react-apollo/client/src/src/Contacts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { gql, graphql } from 'react-apollo';
+import { Link } from 'react-router-dom';
 
 const Contacts = ({ data: { loading, error, contacts }}) => {
   if (loading) {
@@ -10,11 +11,15 @@ const Contacts = ({ data: { loading, error, contacts }}) => {
   }
 
   return (
-    <ul>
-      { contacts.map( item => 
-      (<li key={item.id}>{item.firstName} {item.lastName}</li>)
-      )}
-    </ul>
+    <div className="row">
+      <ul className="collection">
+        { contacts.map( item => 
+        (<li className="collection-item" key={item.id}><Link to={item.id < 0 ? `/` : `contact/${item.id}`}>
+          {item.firstName} {item.lastName}
+          </Link></li>)
+        )}
+      </ul>
+    </div>
   );
 }
 
